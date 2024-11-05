c9c8008710db7f395990084d31a383aeb623347e
LinaRodriguez <lsrodriguez@unillanos.edu.co>
2024-11-05 01:32:33 -0500

    add: landing page

--- movies-frontend/src/App.tsx
+++ movies-frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css'
-import { Layout } from './app/Layout'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <Layout></Layout>
+     <Home/>
     </>
   )
 }
