3be163989fdc9b8bd68f5d716ecb0ba7953947c1
LinaRodriguez <lsrodriguez@unillanos.edu.co>
2024-11-16 15:01:32 -0500

    feat: Sign up & login

---
+++ movies-frontend/src/pages/Explore.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "../components/navigation/Sidebar";
+
+export default function Explore() {
+  return (
+    <>
+      <div className="bg-[#212121]">
+        
+      <Sidebar/>
+      </div>
+      
+    </>
+  )
+}
