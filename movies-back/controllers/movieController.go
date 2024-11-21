e828525f6b4069eea7f0e62400a4f9c4ae627363
LinaRodriguez <lsrodriguez@unillanos.edu.co>
2024-11-21 18:58:16 -0500

    fix json response for /movies

--- movies-back/controllers/movieController.go
+++ movies-back/controllers/movieController.go
@@ -28,7 +28,12 @@ func (c *MovieController) GetMedia(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error fetching media", http.StatusInternalServerError)
 		return
 	}
+	media := movies
+
+	response := map[string]interface{}{
+		"data": media,
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(movies)
-	fmt.Println(json.NewEncoder(w).Encode(movies))
+	json.NewEncoder(w).Encode(response)
 }
